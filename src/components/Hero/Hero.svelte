7de10e9749a2558b825e826e29111ca6491a4a25
joerull-st <joe.rull@startribune.com>
2026-02-25 12:18:02 -0700

    hero stuff

--- src/components/Hero/Hero.svelte
+++ src/components/Hero/Hero.svelte
@@ -54,48 +54,30 @@ Renders a visual replica of the Immersive Template hero.
   } = $props();
 </script>
 
-<Grid additionalClasses="gap-y-8 md:gap-y-10">
-  <GridRow>
-    <div class="flex justify-center my-6">
-      <SectionLabel>{sectionLabel}</SectionLabel>
-    </div>
-    <div class="flex flex-col gap-y-6 md:gap-y-8 text-center">
-      <Headline>{headline}</Headline>
-      <Timestamp>{timestamp}</Timestamp>
-    </div>
-  </GridRow>
+<style>
+  #mydiv { 
+	animation: changeBg 30s infinite;
+	width:100vw;
+	height:80vh;
+	min-height: 800px;
+	background-repeat: no-repeat;
+	background-size:cover;
+  position: relative;
+}
+@keyframes changeBg { 
+   0%,100%  {background-image: url("https://min.merlinone.net/mx/thumbsx3/2026/01/tdir96339/6167_x346_2974_9.jpg");}
+   25% {background-image: url("https://min.merlinone.net/mx/thumbsx3/2026/01/tdir96404/6a78_x316_1edd_9.jpg");}
+   50% {background-image: url("https://min.merlinone.net/mx/thumbsx3/2026/01/tdir96421/1b01_x087_2216_9.jpg");}
+   75% {background-image: url("https://min.merlinone.net/mx/thumbsx3/2026/01/tdir95584/0ca2_x403_1d18_9.jpg");}
+}
+</style>
 
-  {#if heroImageUrl}
-    <GridRow variant={"fullBleed"}>
-      <Image
-        variant={"captionCentered"}
-        src={heroImageUrl}
-        alt={heroImageAltText}
-        caption={heroImageCaption}
-        additionalClasses="!pt-0"
-      />
-    </GridRow>
-  {/if}
-
-  <GridRow
-    additionalClasses="justify-self-center md:max-w-[535px] md:text-center lg:max-w-[712px]"
-  >
-    <div class="flex flex-col gap-y-5 md:justify-items-center md:items-center">
-      <Dek>
-        {dek}
-      </Dek>
-      <div class="flex items-center w-full gap-x-2 md:justify-center">
-        <div class="flex flex-col gap-y-0.5 gap-x-1 md:flex-row">
-          {#if authorName}
-            <div class="flex">
-              <Byline href={authorBioUrl}>By {authorName}</Byline>
-            </div>
-          {/if}
-
-          <span class="text-text-secondary font-utility-label-reg-02">
-            The Minnesota Star Tribune
-          </span>
-        </div>
+<Grid additionalClasses="!max-w-[100vw]">
+  <GridRow additionalClasses="!max-w-[100vw]">
+    <div id='mydiv'>
+      <div class="hero-text">
+        <h1>From ‘The Good Life’ to good neighbors: Minnesota’s ICE resistance rebranded the state </h1>
+        <h2>How Minneapolis’ response to the immigration crackdown forged a new national identity.</h2>
       </div>
     </div>
   </GridRow>
